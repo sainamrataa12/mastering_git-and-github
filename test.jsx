0fd73edb2cbae79d5391387ab064ad8a6e1a2f14
sainamrataa12 <sainamrataachowdary12@gmail.com>
2026-05-26 14:30:08 +0530

    git comands up to pull

--- test.jsx
+++ test.jsx
@@ -1,4 +1,15 @@
 console.log("test")
 //git branch -M main // change branch name to main
 //git remote add origin URL- This will add remote repo so that we can push code to remote repository
-// to add files to remote repository git push -u orgin maingit
+// to add files to remote repository git push -u orgin main
+//we are working on a new feature or updating anything so we create a new branch which creates a copy
+//of our project and we do it by first deciding the source to create branch like generally main or any
+//other branch like git checkout main and then git branch -b branch_name and then git checkout branch_name
+//Now we nake changes and git add ./ then git commit -m 'msg' which saves changes to only local repository.
+//so to add the branch in remote repository we use git push --set-upstream origin branch_name or
+//git push origin branch_name which adds branch and changes to remote repository. From the next time if we
+//make any changes then we just add, comming and push them like git add ./, git commit command and git push command.
+// suppose someone makes changes in your remote branch and you can update your local branch in correspondance to
+// remote branch by using git pull command
+
+
