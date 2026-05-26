698e03e3cbf6816d60e38b58f4e1cb82673d0120
sainamrataa12 <sainamrataachowdary12@gmail.com>
2026-05-26 15:17:53 +0530

    Update test.jsx

--- test.jsx
+++ test.jsx
@@ -10,6 +10,9 @@ console.log("test")
 //git push origin branch_name which adds branch and changes to remote repository. From the next time if we
 //make any changes then we just add, comming and push them like git add ./, git commit command and git push command.
 // suppose someone makes changes in your remote branch and you can update your local branch in correspondance to
-// remote branch by using git pull command
+// remote branch by using git pull command. To merge change in to main branch,  we initiate pull req in main branch. If it is 
+//accepted by all then it is merged to main branch. To merge these changes in local req we use git pull command.
+//Merge conflict- when mul developers tries to change same line of code. It happens. Then git confuses and manually asks developers to decide which change to keep and
+//merge
 
 
